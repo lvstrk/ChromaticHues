28ae2aefd48098e76864bb91bc5e5b5982bda045
lvstrk <ayahage23@gmail.com>
2024-11-09 12:59:40 -0400

    9/11

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 const music = (infomusic) => {
-const img = document.createElement('img')
-img.setAttribute('class', 'song')
+    const img = document.createElement('img')
+    img.setAttribute('class', 'song')
+    img.setAttribute('src', 'https://api.institutoalfa.org/api/songs/image/'+infomusic.image.filename)
 
-img.setAttribute('src', 'assets/Are you bored yet.png')
-
-return img
+    return img
 }
 
 console.log(axios)
 axios.get('https://api.institutoalfa.org/api/songs')
-.then((res) => {
+    .then((res) => {
 
-    console.log(res)
-    
-}) 
+        const canciones = res.data.songs
+
+        canciones.map((canción) => {
+            document.getElementById('container')
+            .appendChild(music(canción))
+        })
+
+    }) 
